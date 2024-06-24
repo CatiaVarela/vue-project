35f254913eabeae484d060f98713a0a4d5f53379
Catia <106531747+CatiaVarela@users.noreply.github.com>
2024-06-24 16:48:39 +0200

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,61 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+<h2> Créer un membre</h2>
+  <form @submit.prevent="soumettreForm">
+    <div class="field is-grouped">
+      <label class="label" for="prenom"> Prénom </label>
+      <input type="text" v-model="prenom" required/>
+
+      <label class="label" for="nom">Nom </label>
+      <input type="text" v-model="nom" required/>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="field is-grouped">
+      <label class="label" for="tel">Téléphone </label>
+      <input type="text" v-model="tel" required/>
+
+      <label class="label" for="mail">Mail </label>
+      <input type="text" v-model="mail" required/>
+    </div>
+    <div class="field is-grouped">
+      <label class="label" for="poste">Poste </label>
+      <input type="text"  v-model="poste" required/>
+
+      <label class="label" for="annee">Année </label>
+      <input type="text" v-model="annee" required/>
+    </div>
+    <button type="submit" class="button is-primary">Soumettre</button>
+  </form>
+
+  <Liste_Membre :membre = "membre"/>
+
 </template>
 
+<script setup>
+import Liste_Membre from "@/components/Liste_Membre.vue";
+import { reactive } from 'vue';
+const membre = reactive({prenom: 'prenom', nom: 'nom', tel: '', mail: '', poste: '', annee: '2024'});
+
+const soumettreForm = () => {
+  console.log(membre);
+}
+
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+
+form{
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  margin: auto;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+form div{
+  margin: 10px;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+h2 {
+  text-align: center;
+  font-size: 64px;
+  color:white;
 }
 </style>
---
+++ src/components/Liste_Membre.vue
@@ -0,0 +1,44 @@
+<template>
+<h2>Liste des membres</h2>
+  <table>
+    <thead>
+      <tr>
+        <th>Prénom</th>
+        <th>Nom</th>
+        <th>Téléphone</th>
+        <th>Mail</th>
+        <th>Poste</th>
+        <th>Année</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>{{ props.membre.prenom }}</td>
+        <td>{{ props.membre.nom }}</td>
+        <td>{{ props.membre.tel }}</td>
+        <td>{{ props.membre.mail }}</td>
+        <td>{{ props.membre.poste }}</td>
+        <td>{{ props.membre.annee }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+const props = defineProps(['membre']);
+
+</script>
+
+<style scoped>
+table {
+  border-collapse: collapse;
+
+}
+
+table, th, td {
+  border: 1px solid #f8f2f2;
+}
+h2 {
+  font-size: 64px;
+}
+</style>
